23baa1d15f46c53e8ef0e86e0a407654ab58025f
Thomas Nesset <tim98@live.no>
2020-09-22 00:38:41 +0200

    Kan legge til fixed employees

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,37 +1,67 @@
 import React, { useState } from "react";
 import { Table, Button, Modal } from "react-bootstrap";
 
-import EmployeeForm from './EmployeeForm'
+import EmployeeForm from "./EmployeeForm";
 
 export default function Employees() {
-  const [employees, setEmployees] = useState([]);
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      firstName: "Thomas",
+      lastName: "Nesset",
+      role: "Frontend",
+    },
+    {
+      id: 2,
+      firstName: "Jonas",
+      lastName: "Say",
+      role: "Backend",
+    },
+  ]);
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const newEmployee = () => {
+      const newEmp = {
+          id: 3,
+          firstName: "Christian",
+          lastName: "Olesen",
+          role: "Graphic Designer"
+      }
+
+    setEmployees((previousEmployeesState) => [
+      newEmp,
+      ...previousEmployeesState,
+    ]);
+  };
+
+  console.log(employees);
+
   return (
     <div>
-      <Table striped bordered hover>
+      <Table striped bordered hover key>
         <thead>
           <tr>
+            <th>ID</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Role</th>
           </tr>
         </thead>
         <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.role}</td>
+            </tr>
+          ))}
           <tr>
-            <td>Thomas</td>
-            <td>Nesset</td>
-            <td>Frontend</td>
-          </tr>
-          <tr>
-            <td>Jonas</td>
-            <td>Say</td>
-            <td>Backend</td>
-          </tr>
-          <tr>
-            <td colSpan="3">
+            <td colSpan="4">
               <Button variant="link" block onClick={handleShow}>
                 Add employee
               </Button>
@@ -41,18 +71,20 @@ export default function Employees() {
       </Table>
 
       <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-              <Modal.Title>New employee</Modal.Title>
-          </Modal.Header>
-          <Modal.Body><EmployeeForm /></Modal.Body>
-          <Modal.Footer>
-              <Button variant='secondary' onClick={handleClose}>
-                  Close
-              </Button>
-              <Button variant='primary' onClick={handleClose}>
-                  Add
-              </Button>
-          </Modal.Footer>
+        <Modal.Header closeButton>
+          <Modal.Title>New employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <EmployeeForm />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => {handleClose(); newEmployee();}}>
+            Add
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
