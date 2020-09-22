5ecee2e5c16baa9073e4e5380385fa9d59fbc6eb
Thomas Nesset <tim98@live.no>
2020-09-22 16:08:43 +0200

    upd

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
-import { Table, Button, Modal } from "react-bootstrap";
-
-import EmployeeForm from "./EmployeeForm";
+import { Table, Button, Modal, Form } from "react-bootstrap";
 
 export default function Employees() {
   const [employees, setEmployees] = useState([
     {
-      id: 1,
+      id: 2,
       firstName: "Thomas",
       lastName: "Nesset",
       role: "Frontend",
     },
     {
-      id: 2,
+      id: 1,
       firstName: "Jonas",
       lastName: "Say",
       role: "Backend",
@@ -25,20 +23,18 @@ export default function Employees() {
   const handleShow = () => setShow(true);
 
   const newEmployee = () => {
-      const newEmp = {
-          id: 3,
-          firstName: "Christian",
-          lastName: "Olesen",
-          role: "Graphic Designer"
-      }
+    const newEmp = {
+      id: 3,
+      firstName: 'Bjørn',
+      lastName: "Olesen",
+      role: "Graphic Designer",
+    };
 
     setEmployees((previousEmployeesState) => [
       newEmp,
       ...previousEmployeesState,
     ]);
   };
-
-  console.log(employees);
 
   return (
     <div>
@@ -75,13 +71,40 @@ export default function Employees() {
           <Modal.Title>New employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <EmployeeForm />
+          <Form>
+            <Form.Group controlId="firstNameForm">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="name" />
+            </Form.Group>
+
+            <Form.Group controlId="lastNameForm">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="name" />
+            </Form.Group>
+
+            <Form.Group controlId="roleForm">
+              <Form.Label>Role</Form.Label>
+              <Form.Control as="select">
+                <option>Frontend</option>
+                <option>Backend</option>
+                <option>Graphic Designer</option>
+                <option>Marketing</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => {handleClose(); newEmployee();}}>
+          <Button
+            type="submit"
+            variant="primary"
+            onClick={() => {
+              handleClose();
+              newEmployee();
+            }}
+          >
             Add
           </Button>
         </Modal.Footer>
