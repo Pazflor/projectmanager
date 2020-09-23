b5c708abd986cfbf2adadaff6991a504e2f6a2e1
Thomas Nesset <tim98@live.no>
2020-09-23 23:22:56 +0200

    Add projects done

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,39 +1,91 @@
-import React, { useState } from 'react'
-import { Table, Button, Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Table, Button, Modal, Form } from "react-bootstrap";
 
 export default function Projects() {
+  const [projectNameInput, setProjectNameInput] = useState("");
+  const [clientNameInput, setClientNameInput] = useState("");
 
-    const [projects, setProjects] = useState([
+  const projectNameHandler = (e) => {
+    setProjectNameInput(e.target.value);
+  };
+
+  const clientNameHandler = (e) => {
+    setClientNameInput(e.target.value);
+  };
+
+  const resetForm = () => {
+    setProjectNameInput("");
+    setClientNameInput("");
+  };
+
+  const newProject = () => {
+    const newPro = {
+      id: projects.length + 1,
+      name: projectNameInput,
+      client: clientNameInput,
+      status: 'In progress..'
+    };
+
+    setProjects((previousProjectsState) => [newPro, ...previousProjectsState]);
+  };
+
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  const handleSubmit = () => {
+    if (projectNameInput === "" || clientNameInput === "") {
+      alert("Fill all fields");
+    } else {
+      setShow(false);
+      newProject();
+    }
+  };
+
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      name: "Test Project",
+      client: "Microsoft",
+      status: "In progress..",
+      team: [
         {
-            id: 1,
-            name: 'test project',
-            client: 'microsoft',
-            team: ['Thomas Nesset', 'Jonas Say']
-        }
-    ])
+          id: 1,
+          name: "Thomas Nesset",
+          role: "Frontend",
+        },
+        {
+          id: 2,
+          name: "Jonas Say",
+          role: "Backend",
+        },
+      ],
+      view: (
+        <Button variant="link" onClick={handleShow}>
+          View Details
+        </Button>
+      ),
+    },
+  ]);
 
-      const [show, setShow] = useState(false);
-      const handleShow = () => setShow(true);
-      const handleClose = () => setShow(false);
-
-    return (
-        <div>
-            <Table striped bordered hover key>
+  return (
+    <div>
+      <Table striped bordered hover key>
         <thead>
           <tr>
             <th>Project ID</th>
             <th>Name</th>
             <th>Client</th>
-            <th>Team</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
-          {projects.map((employee) => (
-            <tr key={employee.id}>
-              <td>{employee.id}</td>
-              <td>{employee.name}</td>
-              <td>{employee.client}</td>
-              <td>{employee.team}</td>
+          {projects.map((project) => (
+            <tr key={project.id}>
+              <td>{project.id}</td>
+              <td>{project.name}</td>
+              <td>{project.client}</td>
+              <td>{project.status}</td>
             </tr>
           ))}
           <tr>
@@ -43,6 +95,7 @@ export default function Projects() {
                 block
                 onClick={() => {
                   handleShow();
+                  resetForm();
                 }}
               >
                 New Project
@@ -51,6 +104,46 @@ export default function Projects() {
           </tr>
         </tbody>
       </Table>
-        </div>
-    )
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Project</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="projectNameForm">
+              <Form.Label>Project name</Form.Label>
+              <Form.Control
+                as="input"
+                type="name"
+                onChange={projectNameHandler}
+              />
+            </Form.Group>
+            <Form.Group controlId="lastNameForm">
+              <Form.Label>Client</Form.Label>
+              <Form.Control
+                as="input"
+                type="name"
+                onChange={clientNameHandler}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            type="submit"
+            variant="primary"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,16 +1,87 @@
 import React, { useState } from "react";
+import { Table, Button, Modal, Card } from "react-bootstrap";
 
 export default function Test() {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
-  const [someText, setSomeText] = useState("Change me!")
-  const changeText = (e) => {
-      setSomeText(e.target.value);
-  }
+  const [statusOption, setStatusOption] = useState("");
+  const statusHandler = (e) => {
+    setStatusOption(e.target.value);
+  };
+
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      name: "Test Project",
+      client: "Microsoft",
+      status: "In progress..",
+      team: [
+        {
+          id: 1,
+          name: "Thomas Nesset",
+          role: "Frontend",
+        },
+        {
+          id: 2,
+          name: "Jonas Say",
+          role: "Backend",
+        },
+      ],
+      view: (
+        <Button variant="link" onClick={handleShow}>
+          View Details
+        </Button>
+      ),
+    },
+  ]);
 
   return (
     <div>
-      <input onChange={changeText}></input>
-      <p>{someText}</p>
+      <Table striped bordered hover key>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Client</th>
+            <th>Status</th>
+            <th>More</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <tr key={project.id}>
+              <td>{project.name}</td>
+              <td>{project.client}</td>
+              <td>
+                <Button variant="warning" disabled>{project.status}</Button>
+              </td>
+              <td>{project.view}</td>
+            </tr>
+          ))}
+          <tr>
+            <td colSpan="5">
+              <Button variant="link" block onClick={() => {}}>
+                New Project
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{projects[0].name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card style={{border: 'none'}}>
+            <Card.Title>Client: {projects[0].client}</Card.Title>
+            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada porta mollis. In sed odio id lectus mattis venenatis. Sed suscipit vehicula sem consequat rutrum. Nulla euismod dignissim neque ornare ultricies. Integer maximus tellus nec placerat vehicula. Sed ultrices ipsum enim. Praesent volutpat condimentum quam, sed imperdiet dolor mattis in.</Card.Text>
+            <Button variant="warning" disabled>{projects[0].status}</Button>
+          </Card>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
     </div>
   );
 }
